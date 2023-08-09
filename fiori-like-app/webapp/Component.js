0911a0643174e33311b8ae82b0439ef19ed39bd8
RohitSM7882 <51930688+RohitSM7882@users.noreply.github.com>
2023-08-09 14:35:54 +0530

    [fix, build]Routing

--- fiori-like-app/webapp/Component.js
+++ fiori-like-app/webapp/Component.js
@@ -4,9 +4,10 @@
 
 sap.ui.define([
         "sap/ui/core/UIComponent",
-        "fiorilikeapp/model/models"
+        "fiorilikeapp/model/models",
+        "sap/m/routing/Target"
     ],
-    function (UIComponent, models) {
+    function (UIComponent, models, Targets) {
         return UIComponent.extend("fiorilikeapp.Component", {
             metadata: {
                 "manifest": "json"
@@ -19,34 +20,34 @@ sap.ui.define([
                 _routerObj.initialize();
             },
             
-            // createContent: function() {
-            //     var oView = new sap.ui.view({
-            //         viewName: "fiorilikeapp.view.App",
-            //         type: "XML",
-            //         id: "idAppView"
-            //     });
+        //     createContent: function() {
+        //         var oView = new sap.ui.view({
+        //             viewName: "fiorilikeapp.view.App",
+        //             type: "XML",
+        //             id: "idAppView"
+        //         });
 
-            //     //this is set at global level, all the views 
-            //     var oModelObj = models.createFruitModel();
-            //     oView.setModel(oModelObj);
+        //         //this is set at global level, all the views 
+        //         var oModelObj = models.createFruitModel();
+        //         oView.setModel(oModelObj);
 
-            //     var _view1 = new sap.ui.view({
-            //         viewName: "fiorilikeapp.view.View1",
-            //         type: "XML",
-            //         id: "idView1"
-            //     });
+        //         var _view1 = new sap.ui.view({
+        //             viewName: "fiorilikeapp.view.View1",
+        //             type: "XML",
+        //             id: "idView1"
+        //         });
 
-            //     var _view2 = new sap.ui.view({
-            //         viewName: "fiorilikeapp.view.Details",
-            //         type: "XML",
-            //         id: "idView2"
-            //     });
+        //         var _view2 = new sap.ui.view({
+        //             viewName: "fiorilikeapp.view.Details",
+        //             type: "XML",
+        //             id: "idView2"
+        //         });
 
-            //     var _appContainer = oView.byId("idAppContainer");
-            //     _appContainer.addMasterPage(_view1).addDetailPage(_view2);
+        //         var _appContainer = oView.byId("idAppContainer");
+        //         _appContainer.addMasterPage(_view1).addDetailPage(_view2);
 
-            //     return oView;
-            // }
+        //         return oView;
+        //     }
         });
     }
 );
